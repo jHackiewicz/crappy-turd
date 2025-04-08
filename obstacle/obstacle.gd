83a696ea0e79f7dfadd143452e3b5f26b0b110ab
Yan Khatskevich <ya.khatskevich@andersenlab.com>
2025-04-08 13:58:57 +0200

    created is turd crash

--- obstacle/obstacle.gd
+++ obstacle/obstacle.gd
@@ -1,6 +1,21 @@
 extends Node2D
+class_name Player
+
+signal on_turd_crash
 
 @export var move_speed: float = 150
 
 func _process(delta: float) -> void:
 	position.x -= move_speed * delta
+
+
+func _on_top_body_entered(body: Node2D) -> void:
+	if not body is Player: return
+	on_turd_crash.emit()
+	print("This works! Top Collision")
+
+
+func _on_bottom_body_entered(body: Node2D) -> void:
+	if not body is Player: return
+	on_turd_crash.emit()
+	print("This works! Bottom Collision")
